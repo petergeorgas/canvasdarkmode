c712d65e2f50a8077e3df4230a777b39e6775461
Peter Georgas <peter_georgas@yahoo.com>
2020-03-21 20:42:48 -0400

    Began testing Marissa's code

--- canvas.js
+++ canvas.js
@@ -5,9 +5,36 @@ function removeThemeColor()
   document.querySelector("[name='theme-color']").remove()
   var head = document.getElementsByTagName('HEAD')[0];
   var meta = document.createElement('meta');
-  meta.setAttribute('name', 'theme-color')
-  meta.content = "#303640";
+  meta.setAttribute('name', 'theme-color');
+  if(chrome.storage.sync.get(['themeToUse'], function(result) {
+      console.log('Value currently is ' + result.themeToUse);
+    }) == "0")
+  {
+    meta.content = "#000";
+  }
+  else
+  {
+    meta.content = "#303640";
+  }
   head.appendChild(meta);
+
 }
 
+function setStyleSheet()
+{
+  var root = document.documentElement;
+
+  if(  chrome.storage.sync.get(['themeToUse'], function(result) {
+      console.log('Value currently is ' + result.themeToUse);
+    }) == "0")
+  {
+    root.style.setProperty('--canvasdarkmode-theme-used',   "#000");
+  }
+  else
+  {
+    root.style.setProperty('--canvasdarkmode-theme-used',   "#303640");
+  }
+}
+
+setStyleSheet();
 removeThemeColor();
